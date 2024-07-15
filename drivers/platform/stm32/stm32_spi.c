Lcompo/no-OS

commit b21737acbcf35879725a75679430bc692ad9dd7d

drivers:platform:stm32: Added SPI DMA implementation
1) Added an spi dma implementation using the DMA drivers

2) A HAL_TIM_MODULE_ENABLED and HAL_DMA_MODULE_ENABLED check
has been enabled so that the HAL specific structures and typedefs
are accessed only when the module is exposed from the .ioc file.

3) Added PWM and DMA remove functions in stm32_spi_remove function

4) Updated the src.mk and stm32_src.mk of relevant projects to
include the DMA/PWM drivers

Signed-off-by: Janani Sunil <janani.sunil@analog.com>

drivers/platform/stm32/stm32_spi.c
@@ -45,6 +45,10 @@
 #include "stm32_spi.h"
 #include "no_os_delay.h"
 #include "no_os_alloc.h"
+#include "stm32_dma.h"
+#ifdef HAL_TIM_MODULE_ENABLED
+#include "no_os_pwm.h"
+#endif
 
 static int stm32_spi_config(struct no_os_spi_desc *desc)
 {
@@ -212,10 +216,47 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 	if (ret)
 		goto error;
 
+	if (sinit->dma_init) {
+		ret = no_os_dma_init(&sdesc->dma_desc, sinit->dma_init);
+		if (ret)
+			goto error;
+
+		if (sinit->rxdma_ch)
+			sdesc->rxdma_ch = sinit->rxdma_ch;
+
+		if (sinit->txdma_ch)
+			sdesc->txdma_ch = sinit->txdma_ch;
+
+		sdesc->dma_desc->sg_handler = sinit->dma_init->sg_handler;
+	}
+
+#ifdef HAL_TIM_MODULE_ENABLED
+	if (sinit->pwm_init) {
+		struct stm32_pwm_desc* spwm_desc;
+		/* Initialize CS PWM */
+		ret = no_os_pwm_init(&sdesc->pwm_desc, sinit->pwm_init);
+		if (ret)
+			goto error;
+
+		spwm_desc = sdesc->pwm_desc->extra;
+		ret = no_os_pwm_disable(sdesc->pwm_desc);
+		if (ret)
+			goto error_pwm;
+
+		spwm_desc->htimer.Instance->CNT = 0;
+	}
+#endif
+
 	*desc = spi_desc;
 
 	return 0;
+
+error_pwm:
+#ifdef HAL_TIM_MODULE_ENABLED
+	no_os_free(sdesc->pwm_desc);
+#endif
 error:
+	no_os_dma_remove(sdesc->dma_desc);
 	no_os_free(spi_desc);
 	no_os_free(sdesc);
 	return ret;
@@ -234,6 +275,12 @@ int32_t stm32_spi_remove(struct no_os_spi_desc *desc)
 		return -EINVAL;
 
 	sdesc = desc->extra;
+#ifdef HAL_TIM_MODULE_ENABLED
+	no_os_pwm_remove(sdesc->pwm_desc);
+#endif
+
+	no_os_dma_remove(sdesc->dma_desc);
+
 #ifdef SPI_SR_TXE
 	__HAL_SPI_DISABLE(&sdesc->hspi);
 #endif
@@ -387,11 +434,138 @@ int32_t stm32_spi_write_and_read(struct no_os_spi_desc *desc,
 }
 
 /**
+ * @brief Configure and start a series of transfers using DMA.
+ * @param desc - The SPI descriptor.
+ * @param msgs - The messages array.
+ * @param len - Number of messages.
+ * @param callback - Function to be invoked after transfers
+ * @param ctx - User defined parameter for the callback function.
+ * @param is_async - Whether or not the function should wait for the completion.
+ * @return 0 in case of success, errno codes otherwise.
+ */
+int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
+				   struct no_os_spi_msg* msgs,
+				   uint32_t len,
+				   void (*callback)(void*),
+				   void* ctx, bool is_async)
+{
+	struct stm32_spi_desc* sdesc = desc->extra;
+	struct no_os_dma_xfer_desc* rx_ch_xfer;
+	struct no_os_dma_xfer_desc* tx_ch_xfer;
+	struct no_os_dma_ch* tx_ch = sdesc->txdma_ch;
+	struct no_os_dma_ch* rx_ch = sdesc->rxdma_ch;
+	struct stm32_dma_channel* sdma_rx = rx_ch->extra;
+	struct stm32_dma_channel* sdma_tx = tx_ch->extra;
+	SPI_TypeDef* SPIx = sdesc->hspi.Instance;
+	int ret;
+	uint8_t i;
+
+	if (!desc || !msgs)
+		return -EINVAL;
+
+	rx_ch_xfer = no_os_calloc(len, sizeof(*rx_ch_xfer));
+	if (!rx_ch_xfer)
+		return -ENOMEM;
+
+	tx_ch_xfer = no_os_calloc(len, sizeof(*tx_ch_xfer));
+	if (!tx_ch_xfer) {
+		goto free_rx_ch_xfer;
+	}
+
+	for (i = 0; i < len; i++) {
+		tx_ch_xfer[i].src = msgs[i].tx_buff;
+		tx_ch_xfer[i].dst = &(SPIx->DR);
+		tx_ch_xfer[i].xfer_type = MEM_TO_DEV;
+		tx_ch_xfer[i].periph = NO_OS_DMA_IRQ;
+		tx_ch_xfer[i].length = msgs[i].bytes_number;
+
+		rx_ch_xfer[i].dst = msgs[i].rx_buff;
+		rx_ch_xfer[i].src = &(SPIx->DR);
+		rx_ch_xfer[i].periph = NO_OS_DMA_IRQ;
+		rx_ch_xfer[i].xfer_type = DEV_TO_MEM;
+		rx_ch_xfer[i].length = msgs[i].bytes_number;
+		if (callback) {
+			sdesc->dma_desc->sg_handler = callback;
+		}
+	}
+
+	rx_ch->id = sdma_rx->hdma;
+	rx_ch->sg_list = NULL;
+
+	ret = no_os_dma_config_xfer(sdesc->dma_desc, rx_ch_xfer, len, rx_ch);
+	if (ret)
+		goto remove_dma;
+
+	tx_ch->id = sdma_tx->hdma;
+	tx_ch->sg_list = NULL;
+
+	ret = no_os_dma_config_xfer(sdesc->dma_desc, tx_ch_xfer, len, tx_ch);
+	if (ret)
+		goto remove_dma;
+
+	ret = no_os_dma_xfer_start(sdesc->dma_desc, tx_ch);
+	if (ret)
+		goto abort_transfer;
+
+	ret = no_os_dma_xfer_start(sdesc->dma_desc, rx_ch);
+	if (ret)
+		goto abort_transfer;
+
+	if (tx_ch)
+		SET_BIT(sdesc->hspi.Instance->CR2, SPI_CR2_TXDMAEN);
+
+	if (rx_ch)
+		SET_BIT(sdesc->hspi.Instance->CR2, SPI_CR2_RXDMAEN);
+
+#ifdef HAL_TIM_MODULE_ENABLED
+	if (sdesc->pwm_desc) {
+		ret = no_os_pwm_enable(sdesc->pwm_desc);
+		if (ret)
+			goto abort_transfer;
+	}
+#endif
+
+	return 0;
+
+abort_transfer:
+	no_os_dma_xfer_abort(sdesc->dma_desc, tx_ch);
+	no_os_dma_xfer_abort(sdesc->dma_desc, rx_ch);
+remove_dma:
+	no_os_dma_remove(sdesc->dma_desc);
+free_tx_ch_xfer:
+	no_os_free(tx_ch_xfer);
+free_rx_ch_xfer:
+	no_os_free(rx_ch_xfer);
+
+	return ret;
+}
+
+/**
+ * @brief Configure and start a series of transfers using DMA. Wait for the
+ * 	  completion before returning.
+ * @param desc - The SPI descriptor.
+ * @param msgs - The messages array.
+ * @param len - Number of messages.
+ * @param callback - Function to be invoked once the transfers are done.
+ * @param ctx - User defined parameter for the callback function.
+ * @return 0 in case of success, errno codes otherwise.
+ */
+int32_t stm32_spi_dma_transfer_async(struct no_os_spi_desc* desc,
+				     struct no_os_spi_msg* msgs,
+				     uint32_t len,
+				     void (*callback)(void*),
+				     void* ctx)
+{
+	return stm32_config_dma_and_start(desc, msgs, len, callback, ctx, true);
+}
+
+/**
  * @brief stm32 platform specific SPI platform ops structure
  */
 const struct no_os_spi_platform_ops stm32_spi_ops = {
 	.init = &stm32_spi_init,
 	.write_and_read = &stm32_spi_write_and_read,
 	.remove = &stm32_spi_remove,
-	.transfer = &stm32_spi_transfer
+	.transfer = &stm32_spi_transfer,
+	.dma_transfer_async = &stm32_spi_dma_transfer_async
 };

drivers/platform/stm32/stm32_spi.h
@@ -41,7 +41,11 @@
 
 #include <stdint.h>
 #include "no_os_spi.h"
+#include "no_os_dma.h"
 #include "stm32_hal.h"
+#ifdef HAL_TIM_MODULE_ENABLED
+#include "stm32_pwm.h"
+#endif
 
 /**
  * @struct stm32_spi_init_param
@@ -53,6 +57,16 @@ struct stm32_spi_init_param {
 	uint32_t chip_select_port;
 	/** Get perihperal source clock function */
 	uint32_t (*get_input_clock)(void);
+	/** DMA Initialization Parameters */
+	struct no_os_dma_init_param* dma_init;
+	/** RX DMA Channel Descriptor */
+	struct no_os_dma_ch* rxdma_ch;
+	/** TX DMA Channel Descriptor */
+	struct no_os_dma_ch* txdma_ch;
+#ifdef HAL_TIM_MODULE_ENABLED
+	/** CS PWM Initialization Parameters */
+	const struct no_os_pwm_init *pwm_init;
+#endif
 };
 
 /**
@@ -66,6 +80,16 @@ struct stm32_spi_desc {
 	uint32_t input_clock;
 	/** Chip select gpio descriptor */
 	struct no_os_gpio_desc *chip_select;
+	/** DMA Descriptor */
+	struct no_os_dma_desc* dma_desc;
+	/** RX DMA Channel Descriptor */
+	struct no_os_dma_ch* rxdma_ch;
+	/** TX DMA Channel Descriptor */
+	struct no_os_dma_ch* txdma_ch;
+#ifdef HAL_TIM_MODULE_ENABLED
+	/** CS PWM descriptor */
+	struct no_os_pwm_desc* pwm_desc;
+#endif
 };
 
 /**

projects/ad738x-st/src.mk
@@ -28,6 +28,7 @@ INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_crc8.h \
 		$(INCLUDE)/no_os_alloc.h \
 		$(INCLUDE)/no_os_mutex.h \
+		$(INCLUDE)/no_os_dma.h \
 		$(INCLUDE)/no_os_circular_buffer.h
 
 SRCS += $(DRIVERS)/api/no_os_gpio.c \
@@ -42,6 +43,7 @@ SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(NO-OS)/util/no_os_crc8.c \
 		$(NO-OS)/util/no_os_alloc.c \
 		$(NO-OS)/util/no_os_mutex.c \
+		$(DRIVERS)/api/no_os_dma.c \
 		$(NO-OS)/util/no_os_circular_buffer.c
 
 INCS += $(DRIVERS)/adc/ad738x/ad738x.h

projects/ad738x-st/src/platform/stm32/platform_src.mk
@@ -5,11 +5,13 @@ INCS += $(PLATFORM_DRIVERS)/stm32_delay.h     \
 	$(PLATFORM_DRIVERS)/stm32_irq.h       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.h  \
 	$(PLATFORM_DRIVERS)/stm32_uart.h      \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h
+	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
+	$(PLATFORM_DRIVERS)/stm32_dma.h
 
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c     \
 	$(PLATFORM_DRIVERS)/stm32_gpio.c      \
 	$(PLATFORM_DRIVERS)/stm32_spi.c       \
+	$(PLATFORM_DRIVERS)/stm32_dma.c       \
 	$(PLATFORM_DRIVERS)/stm32_irq.c       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.c  \
 	$(PLATFORM_DRIVERS)/stm32_uart.c      \

projects/ad74413r/src/platform/stm32/platform_src.mk
@@ -5,7 +5,8 @@ INCS += $(PLATFORM_DRIVERS)/stm32_delay.h     \
 	$(PLATFORM_DRIVERS)/stm32_irq.h       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.h  \
 	$(PLATFORM_DRIVERS)/stm32_uart.h      \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h
+	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
+	$(PLATFORM_DRIVERS)/stm32_dma.h
 
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c     \
 	$(PLATFORM_DRIVERS)/stm32_gpio.c      \
@@ -13,4 +14,5 @@ SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c     \
 	$(PLATFORM_DRIVERS)/stm32_irq.c       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.c  \
 	$(PLATFORM_DRIVERS)/stm32_uart.c      \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c
+	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c \
+	$(PLATFORM_DRIVERS)/stm32_dma.c

projects/ad74416h/src.mk
@@ -27,7 +27,8 @@ INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_init.h \
 		$(INCLUDE)/no_os_crc8.h \
 		$(INCLUDE)/no_os_alloc.h \
-		$(INCLUDE)/no_os_mutex.h
+		$(INCLUDE)/no_os_mutex.h \
+		$(INCLUDE)/no_os_dma.h
 
 SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(DRIVERS)/api/no_os_i2c.c  \
@@ -40,7 +41,8 @@ SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(NO-OS)/util/no_os_util.c \
 		$(NO-OS)/util/no_os_crc8.c \
 		$(NO-OS)/util/no_os_alloc.c \
-		$(NO-OS)/util/no_os_mutex.c
+		$(NO-OS)/util/no_os_mutex.c \
+		$(DRIVERS)/api/no_os_dma.c
 
 INCS += $(DRIVERS)/adc-dac/ad74416h/ad74416h.h
 SRCS += $(DRIVERS)/adc-dac/ad74416h/ad74416h.c

projects/ad74416h/src/platform/stm32/platform_src.mk
@@ -2,6 +2,7 @@ INCS += $(PLATFORM_DRIVERS)/stm32_delay.h     \
 	$(PLATFORM_DRIVERS)/stm32_gpio.h      \
 	$(PLATFORM_DRIVERS)/stm32_hal.h       \
 	$(PLATFORM_DRIVERS)/stm32_spi.h       \
+	$(PLATFORM_DRIVERS)/stm32_dma.h       \
 	$(PLATFORM_DRIVERS)/stm32_irq.h       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.h  \
 	$(PLATFORM_DRIVERS)/stm32_uart.h      \
@@ -10,6 +11,7 @@ INCS += $(PLATFORM_DRIVERS)/stm32_delay.h     \
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c     \
 	$(PLATFORM_DRIVERS)/stm32_gpio.c      \
 	$(PLATFORM_DRIVERS)/stm32_spi.c       \
+	$(PLATFORM_DRIVERS)/stm32_dma.c       \
 	$(PLATFORM_DRIVERS)/stm32_irq.c       \
 	$(PLATFORM_DRIVERS)/stm32_gpio_irq.c  \
 	$(PLATFORM_DRIVERS)/stm32_uart.c      \

projects/ad7616-st/src.mk
@@ -17,6 +17,7 @@ INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_i2c.h       \
 		$(INCLUDE)/no_os_print_log.h \
 		$(INCLUDE)/no_os_spi.h       \
+		$(INCLUDE)/no_os_dma.h       \
 		$(INCLUDE)/no_os_irq.h      \
 		$(INCLUDE)/no_os_list.h      \
 		$(INCLUDE)/no_os_timer.h      \
@@ -35,6 +36,7 @@ SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(NO-OS)/util/no_os_lf256fifo.c \
 		$(DRIVERS)/api/no_os_irq.c  \
 		$(DRIVERS)/api/no_os_spi.c  \
+		$(DRIVERS)/api/no_os_dma.c  \
 		$(DRIVERS)/api/no_os_timer.c  \
 		$(DRIVERS)/api/no_os_uart.c \
 		$(NO-OS)/util/no_os_list.c \

commit a7639783db8b6ce7ec3efca384a564f062be4a35

platform: stm32: spi: Fix pmw remove on error
The driver should call no_os_pwm_remove not free to release the pwm.

Fixes: b21737acbc ("drivers:platform:stm32: Added SPI DMA implementation")
Signed-off-by: Axel Haslam <ahaslam@baylibre.com>

drivers/platform/stm32/stm32_spi.c
@@ -253,7 +253,7 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 
 error_pwm:
 #ifdef HAL_TIM_MODULE_ENABLED
-	no_os_free(sdesc->pwm_desc);
+	no_os_pwm_remove(sdesc->pwm_desc);
 #endif
 error:
 	no_os_dma_remove(sdesc->dma_desc);