62ab958e737e7d42eaf65923831633e9d7d2a6a3
Louijie Compo <louijie.compo@analog.com>
2025-03-06 10:48:21 +0800

    CHanged to BSD 3 clause. Removed contributions. Changed ret condition, removed | operator.

--- drivers/magnetometer/admt4000/admt4000.c
+++ drivers/magnetometer/admt4000/admt4000.c
@@ -4,43 +4,34 @@
  *   @author Marc Sosa (marcpaolo.sosa@analog.com)
  *   @author Jose Ramon San Buenaventura (jose.sanbuenaventura@analog.com)
 ********************************************************************************
- * Copyright 2024(c) Analog Devices, Inc.
- *
- * All rights reserved.
+ * Copyright 2020(c) Analog Devices, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *  - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *  - Neither the name of Analog Devices, Inc. nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *  - The use of this software may or may not infringe the patent rights
- *    of one or more patent holders.  This license does not release you
- *    from the requirement that you obtain separate licenses from these
- *    patent holders to use this software.
- *  - Use of the software either in source or binary form, must be run
- *    on or directly connected to an Analog Devices Inc. component.
  *
- * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
-/******************************************************************************/
-/***************************** Include Files **********************************/
-/******************************************************************************/
 #include <errno.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -51,10 +42,7 @@
 #include "no_os_alloc.h"
 #include "no_os_delay.h"
 
-/******************************************************************************/
-/************************ Functions Definitions *******************************/
-/******************************************************************************/
-/******************************************************************************/
+
 static uint8_t admt4000_ecc_control_registers[] = {
     ADMT4000_02_REG_GENERAL,
     ADMT4000_02_REG_DIGIOEN,
@@ -111,32 +99,32 @@ int admt4000_init(struct admt4000_dev **device,
     /**  Get Descriptors **/
     ret = no_os_gpio_get(&dev->gpio_coil_rs,
                          &init_param.gpio_coil_rs);
-    if (ret < 0)
+    if (ret)
         goto err;
 
     ret = no_os_gpio_get(&dev->gpio_gpio0_busy,
                          &init_param.gpio_gpio0_busy);
-    if (ret < 0)
+    if (ret)
         goto err;
 
     ret = no_os_gpio_get(&dev->gpio_shdn_n,
                          &init_param.gpio_shdn_n);
-    if (ret < 0)
+    if (ret)
         goto err;
 
     /** Set Direction **/
-    ret |= no_os_gpio_direction_output(dev->gpio_coil_rs,
+    ret = no_os_gpio_direction_output(dev->gpio_coil_rs,
                                        NO_OS_GPIO_LOW);
-    if (ret < 0)
+    if (ret)
         goto err;
 
-    ret |= no_os_gpio_direction_input(dev->gpio_gpio0_busy);
-    if (ret < 0)
+    ret = no_os_gpio_direction_input(dev->gpio_gpio0_busy);
+    if (ret)
         goto err;
 
-    ret |= no_os_gpio_direction_output(dev->gpio_shdn_n,
+    ret = no_os_gpio_direction_output(dev->gpio_shdn_n,
                                        NO_OS_GPIO_HIGH);
-    if (ret < 0)
+    if (ret)
         goto err;
 
     /* Delay */
