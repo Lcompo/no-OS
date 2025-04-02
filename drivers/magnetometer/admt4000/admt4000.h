04ab3bc807e0fffca74d7f9bb60a25ab6ba94763
Louijie Compo <louijie.compo@analog.com>
2025-04-02 14:25:30 +0800

    applied astyle again

--- drivers/magnetometer/admt4000/admt4000.h
+++ drivers/magnetometer/admt4000/admt4000.h
@@ -220,138 +220,138 @@ extern "C" {
 #define ADMT4000_HPHA_RES               878910 // (0.087891 * ADMT4000_SF) // hpha resolution
 
 enum admt4000_faults {
-    ADMT4000_FAULT_VDD_UV,
-    ADMT4000_FAULT_VDD_OV,
-    ADMT4000_FAULT_VDRIVE_UV,
-    ADMT4000_FAULT_VDRIVE_OV,
-    ADMT4000_FAULT_AFEDIAG,
-    ADMT4000_FAULT_NVM_CRC,
-    ADMT4000_FAULT_RESERVED,
-    ADMT4000_FAULT_ECC_DOUBLE,
-    ADMT4000_FAULT_OSC_DRIFT,
-    ADMT4000_FAULT_GMR_REF_RES,
-    ADMT4000_FAULT_ANGLESEC_RANGE,
-    ADMT4000_FAULT_GMR_ELEMENTS,
-    ADMT4000_FAULT_MTDIAG,
-    ADMT4000_FAULT_TURN_CTRS,
-    ADMT4000_FAULT_AMR_RAD_AMP,
-    ADMT4000_FAULT_SEQ_WATCHDOG,
+	ADMT4000_FAULT_VDD_UV,
+	ADMT4000_FAULT_VDD_OV,
+	ADMT4000_FAULT_VDRIVE_UV,
+	ADMT4000_FAULT_VDRIVE_OV,
+	ADMT4000_FAULT_AFEDIAG,
+	ADMT4000_FAULT_NVM_CRC,
+	ADMT4000_FAULT_RESERVED,
+	ADMT4000_FAULT_ECC_DOUBLE,
+	ADMT4000_FAULT_OSC_DRIFT,
+	ADMT4000_FAULT_GMR_REF_RES,
+	ADMT4000_FAULT_ANGLESEC_RANGE,
+	ADMT4000_FAULT_GMR_ELEMENTS,
+	ADMT4000_FAULT_MTDIAG,
+	ADMT4000_FAULT_TURN_CTRS,
+	ADMT4000_FAULT_AMR_RAD_AMP,
+	ADMT4000_FAULT_SEQ_WATCHDOG,
 };
 
 enum admt4000_vdd {
-    ADMT4000_3P3V,
-    ADMT4000_5V,
+	ADMT4000_3P3V,
+	ADMT4000_5V,
 };
 
 enum admt4000_cnv_sync {
-    ADMT4000_SEQ_START,
-    ADMT4000_SYNC_EDGE = 0x3,
+	ADMT4000_SEQ_START,
+	ADMT4000_SYNC_EDGE = 0x3,
 };
 
 enum admt4000_seq_mode {
-    ADMT4000_MODE2,
-    ADMT4000_MODE1 = 0x3,
+	ADMT4000_MODE2,
+	ADMT4000_MODE1 = 0x3,
 };
 
 enum admt4000_angle_type {
-    ADMT4000_RAW_SINE,
-    ADMT4000_RAW_COSINE,
-    ADMT4000_RAW_SECANGLI,
-    ADMT4000_RAW_SECANGLEQ,
+	ADMT4000_RAW_SINE,
+	ADMT4000_RAW_COSINE,
+	ADMT4000_RAW_SECANGLI,
+	ADMT4000_RAW_SECANGLEQ,
 };
 
 enum admt400_angle_eck_type {
-    ADMT4000_ALWAYS_FLAG,
-    ADMT4000_VALID,
-    ADMT4000_DISABLE_CHECK,
+	ADMT4000_ALWAYS_FLAG,
+	ADMT4000_VALID,
+	ADMT4000_DISABLE_CHECK,
 };
 
 struct admt4000_gpio {
-    bool logic_state;
-    bool is_alt_pin;
+	bool logic_state;
+	bool is_alt_pin;
 };
 
 struct admt4000_dev {
-    struct no_os_spi_desc *spi_desc;
-    struct no_os_gpio_desc *gpio_coil_rs;
-    struct no_os_gpio_desc *gpio_gpio0_busy;
-    struct no_os_gpio_desc *gpio_shdn_n;
+	struct no_os_spi_desc *spi_desc;
+	struct no_os_gpio_desc *gpio_coil_rs;
+	struct no_os_gpio_desc *gpio_gpio0_busy;
+	struct no_os_gpio_desc *gpio_shdn_n;
 
-    /* Boolean indicator if setup in page 0 or not */
-    bool is_page_zero;
+	/* Boolean indicator if setup in page 0 or not */
+	bool is_page_zero;
 
-    /* Tracker for CNV bits if falling or rising */
-    bool edge;
+	/* Tracker for CNV bits if falling or rising */
+	bool edge;
 
-    /* Contains GPIO information: logic level and function*/
-    struct admt4000_gpio gpios[6];
+	/* Contains GPIO information: logic level and function*/
+	struct admt4000_gpio gpios[6];
 
-    /* VDD Setting indicator: 5V or 3.3V variant only */
-    enum admt4000_vdd dev_vdd;
+	/* VDD Setting indicator: 5V or 3.3V variant only */
+	enum admt4000_vdd dev_vdd;
 
-    /* Tracker for conversion type: one shot or continuous */
-    bool is_one_shot;
+	/* Tracker for conversion type: one shot or continuous */
+	bool is_one_shot;
 
-    /* Throws early samples of a channel*/
-    bool is_throw_early_samples;
+	/* Throws early samples of a channel*/
+	bool is_throw_early_samples;
 
-    /* Conversion factor for fixed voltage measurements (uV) */
-    uint32_t fixed_conv_factor_mv;
+	/* Conversion factor for fixed voltage measurements (uV) */
+	uint32_t fixed_conv_factor_mv;
 
-    /* Tracker for type of angle threshold set */
-    enum admt400_angle_eck_type eck_type;
+	/* Tracker for type of angle threshold set */
+	enum admt400_angle_eck_type eck_type;
 };
 
 struct admt4000_init_param {
-    struct no_os_spi_init_param spi_init;
-    struct no_os_gpio_init_param gpio_coil_rs;
-    struct no_os_gpio_init_param gpio_gpio0_busy;
-    struct no_os_gpio_init_param gpio_shdn_n;
+	struct no_os_spi_init_param spi_init;
+	struct no_os_gpio_init_param gpio_coil_rs;
+	struct no_os_gpio_init_param gpio_gpio0_busy;
+	struct no_os_gpio_init_param gpio_shdn_n;
 
-    /* Specifier for ADMT4000 variant based on VDD */
-    enum admt4000_vdd dev_vdd;
+	/* Specifier for ADMT4000 variant based on VDD */
+	enum admt4000_vdd dev_vdd;
 };
 
 int test(char* in_buf, char*out_buf);
 
 /* Initializes the device structure */
 int admt4000_init(struct admt4000_dev **device,
-                  struct admt4000_init_param init_param);
+		  struct admt4000_init_param init_param);
 
 /* Deallocates the resources for the device structure */
 int admt4000_remove(struct admt4000_dev *dev);
 
 /* Compute CRC for given data stream */
 int admt4000_compute_crc(long reg_addr, uint16_t reg_data, uint8_t excess,
-                         bool is_write, uint8_t *crc_ret);
+			 bool is_write, uint8_t *crc_ret);
 
 /* Encodes the input data using ECC algorithm of admt4000 */
 int admt4000_ecc_encode(uint8_t *parity_num, uint8_t *code_length,
-                        uint8_t *code, uint8_t *input, uint8_t size_code, uint8_t size_input,
-                        uint8_t *ecc);
+			uint8_t *code, uint8_t *input, uint8_t size_code, uint8_t size_input,
+			uint8_t *ecc);
 
 /* Hamming Calculator to generate the bit value during encoding */
 int admt4000_hamming_calc(uint8_t position,
-                          uint8_t code_length, uint8_t *code);
+			  uint8_t code_length, uint8_t *code);
 
 /* Performs error checking on sequence of interest i.e. ECC bytes */
 int admt4000_error_check(uint8_t parity_num,
-                         uint8_t code_length, uint8_t *code, uint8_t *error_pos);
+			 uint8_t code_length, uint8_t *code, uint8_t *error_pos);
 
 /* Update ECC contents */
 int admt4000_update_ecc(struct admt4000_dev *device, uint16_t *ecc_val);
 
 /* Read from selected register */
 int admt4000_read(struct admt4000_dev *device, uint8_t reg_addr,
-                  uint16_t *reg_data, uint8_t *verif);
+		  uint16_t *reg_data, uint8_t *verif);
 
 /* Write to selected register */
 int admt4000_write(struct admt4000_dev *device, uint8_t reg_addr,
-                   uint16_t reg_data);
+		   uint16_t reg_data);
 
 /* Update register fields */
 int admt4000_reg_update(struct admt4000_dev *device, uint8_t reg_addr,
-                        uint16_t update_mask, uint16_t update_val);
+			uint16_t update_mask, uint16_t update_val);
 
 /* Switch the device to either page zero or page two */
 int admt4000_set_page(struct admt4000_dev *device, bool is_page_zero);
@@ -369,8 +369,9 @@ int admt4000_get_cnv(struct admt4000_dev *device, bool *is_rising);
 int admt4000_raw_angle_read(struct admt4000_dev *device, uint16_t *angle_data);
 
 /* Parses raw data from ABSANGLE and ANGLE registers */
-int admt4000_get_raw_turns_and_angle(struct admt4000_dev *device, uint8_t *turns,
-                                     uint16_t *angle);
+int admt4000_get_raw_turns_and_angle(struct admt4000_dev *device,
+				     uint8_t *turns,
+				     uint16_t *angle);
 
 /* Get selected GPIO's logic level */
 int admt4000_get_gpio(struct admt4000_dev *device, uint8_t gpio, bool *logic);
@@ -383,51 +384,54 @@ int admt4000_clear_all_faults(struct admt4000_dev *device);
 
 /* Reads selected fault's status */
 int admt4000_read_fault(struct admt4000_dev *device, enum admt4000_faults fault,
-                        bool *is_flagged);
+			bool *is_flagged);
 
 /* Get secondary angle raw data */
 int admt4000_get_secondary_angle(struct admt4000_dev *device, uint16_t *angle);
 
 /* Get Secondary Angle Data (Converted, degrees) */
-int admt4000_get_converted_secondary_angle(struct admt4000_dev *device, uint32_t *angle);
+int admt4000_get_converted_secondary_angle(struct admt4000_dev *device,
+		uint32_t *angle);
 
 /* Get angle raw data */
 int admt4000_get_angle(struct admt4000_dev *device, uint16_t *raw_angle,
-                       enum admt4000_angle_type angle_type);
+		       enum admt4000_angle_type angle_type);
 
 /* Get radius raw data */
 int admt4000_get_radius(struct admt4000_dev *device, uint16_t *radius);
 
 /* Get Converted Radius Data (mV/V) */
-int admt4000_get_converted_radius(struct admt4000_dev *device, uint32_t *radius);
+int admt4000_get_converted_radius(struct admt4000_dev *device,
+				  uint32_t *radius);
 
 /* Obtain status of selected reference resistor (GMR turn count) */
 int admt4000_get_ref_res_state(struct admt4000_dev *device, uint8_t ref_res_num,
-                               bool *is_ok);
+			       bool *is_ok);
 
 /* Get fixed value voltage to GMR turn count sensor */
 int admt4000_get_fixed_voltage(struct admt4000_dev *device,
-                               uint8_t *fixed_volt);
+			       uint8_t *fixed_volt);
 
 /* Get fixed value voltage (Volts) */
 int admt4000_get_converted_fixed_voltage(struct admt4000_dev *device,
-        uint32_t *fixed_volt);
+		uint32_t *fixed_volt);
 
 /* Read from either + or - 57% (or both) diagnostic resistor channels */
 int admt4000_get_diag_res(struct admt4000_dev *device, uint16_t *diag_meas,
-                          uint8_t is_pos);
+			  uint8_t is_pos);
 
 /* Read percentage value from diagnostic resistors */
-int admt4000_get_converted_diag_res(struct admt4000_dev *device, uint32_t *diag_meas,
-                                    uint8_t is_pos);
+int admt4000_get_converted_diag_res(struct admt4000_dev *device,
+				    uint32_t *diag_meas,
+				    uint8_t is_pos);
 
 /* Read raw temperature (primary or secondary) */
 int admt4000_get_temp(struct admt4000_dev *device, uint16_t *temp,
-                      bool is_primary);
+		      bool is_primary);
 
 /* Read converted temperature */
 int admt4000_get_converted_temp(struct admt4000_dev *device, uint32_t *temp,
-                                bool is_primary);
+				bool is_primary);
 
 /* Obtain the 8 bit equivalent storage bits from general config register */
 int admt4000_get_storage_config(struct admt4000_dev *device, uint8_t *storage);
@@ -437,11 +441,11 @@ int admt4000_set_storage_config(struct admt4000_dev *device, uint8_t storage);
 
 /* Get conversion synchronization mode from general config register */
 int admt4000_get_cnv_sync(struct admt4000_dev *device,
-                          enum admt4000_cnv_sync *sync);
+			  enum admt4000_cnv_sync *sync);
 
 /* Set conversion synchronization mode */
 int admt4000_set_cnv_sync(struct admt4000_dev *device,
-                          enum admt4000_cnv_sync sync);
+			  enum admt4000_cnv_sync sync);
 
 /* Check if filter is enabled on angle conversions */
 int admt4000_get_angle_filt(struct admt4000_dev *device, bool *is_filtered);
@@ -457,11 +461,11 @@ int admt4000_set_h8_ctrl(struct admt4000_dev *device, bool is_user_supplied);
 
 /* Get the sequencer mode configuration */
 int admt4000_get_seq_mode(struct admt4000_dev *device,
-                          enum admt4000_seq_mode *seq);
+			  enum admt4000_seq_mode *seq);
 
 /* Set the sequencer mode configuration */
 int admt4000_set_seq_mode(struct admt4000_dev *device,
-                          enum admt4000_seq_mode seq);
+			  enum admt4000_seq_mode seq);
 
 /* Get conversion mode configuration */
 int admt4000_get_cnv_mode(struct admt4000_dev *device, bool *is_one_shot);
@@ -474,19 +478,21 @@ int admt4000_io_en(struct admt4000_dev *device, uint8_t gpio, bool is_en);
 
 /* Configures between alt function of GPIO function for selected GPIO */
 int admt4000_gpio_func(struct admt4000_dev *device, uint8_t gpio,
-                       bool is_alt_func);
+		       bool is_alt_func);
 
 /* Get angle threshold value (Raw) */
 int admt4000_get_angle_thres(struct admt4000_dev *device, uint16_t *thres);
 
 /* Get angle threshold value (Degrees) */
-int admt4000_get_converted_angle_thres(struct admt4000_dev *device, uint32_t *thres);
+int admt4000_get_converted_angle_thres(struct admt4000_dev *device,
+				       uint32_t *thres);
 
 /* Set angle threshold value (Raw) */
 int admt4000_set_angle_thres(struct admt4000_dev *device, uint16_t thres);
 
 /* Set angle threshold value (Degrees) */
-int admt4000_set_converted_angle_thres(struct admt4000_dev *device, uint32_t thres);
+int admt4000_set_converted_angle_thres(struct admt4000_dev *device,
+				       uint32_t thres);
 
 /* Get conversion count value */
 int admt4000_get_cnv_cnt(struct admt4000_dev *device, uint8_t *cnt);
@@ -495,28 +501,36 @@ int admt4000_get_cnv_cnt(struct admt4000_dev *device, uint8_t *cnt);
 int admt4000_set_cnv_cnt(struct admt4000_dev *device, uint8_t cnt);
 
 /* Get the Nth harmonic error magnitude (raw) */
-int admt4000_get_hmag_config(struct admt4000_dev *device, uint8_t hmag, uint16_t *mag);
+int admt4000_get_hmag_config(struct admt4000_dev *device, uint8_t hmag,
+			     uint16_t *mag);
 
 /* Get the Nth harmonic error magnitude (converted) */
-int admt4000_get_converted_hmag_config(struct admt4000_dev *device, uint8_t hmag, uint32_t *mag);
+int admt4000_get_converted_hmag_config(struct admt4000_dev *device,
+				       uint8_t hmag, uint32_t *mag);
 
 /* Set the Nth harmonic error magnitude (raw) */
-int admt4000_set_hmag_config(struct admt4000_dev *device, uint8_t hmag, uint16_t mag);
+int admt4000_set_hmag_config(struct admt4000_dev *device, uint8_t hmag,
+			     uint16_t mag);
 
 /* Set the Nth harmonic error magnitude (converted) */
-int admt4000_set_converted_hmag_config(struct admt4000_dev *device, uint8_t hmag, uint32_t mag);
+int admt4000_set_converted_hmag_config(struct admt4000_dev *device,
+				       uint8_t hmag, uint32_t mag);
 
 /* Get the Nth harmonic error phase (Raw) */
-int admt4000_get_hphase_config(struct admt4000_dev *device, uint8_t hpha, uint16_t *pha);
+int admt4000_get_hphase_config(struct admt4000_dev *device, uint8_t hpha,
+			       uint16_t *pha);
 
 /* Get the Nth harmonic error phase (Converted) */
-int admt4000_get_converted_hphase_config(struct admt4000_dev *device, uint8_t hpha, uint32_t *pha);
+int admt4000_get_converted_hphase_config(struct admt4000_dev *device,
+		uint8_t hpha, uint32_t *pha);
 
 /* Set the Nth harmonic error phase (Raw) */
-int admt4000_set_hphase_config(struct admt4000_dev *device, uint8_t hpha, uint16_t pha);
+int admt4000_set_hphase_config(struct admt4000_dev *device, uint8_t hpha,
+			       uint16_t pha);
 
 /* Set the Nth harmonic error phase (Converted) */
-int admt4000_set_converted_hphase_config(struct admt4000_dev *device, uint8_t hpha, uint32_t pha);
+int admt4000_set_converted_hphase_config(struct admt4000_dev *device,
+		uint8_t hpha, uint32_t pha);
 
 /* Read selected device specific ID's */
 int admt4000_get_id(struct admt4000_dev *device, uint8_t id_num, uint16_t *id);
